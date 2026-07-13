79e6f4f8b38404cc2b270e8ad753d74d261e1f45
zm23237 <zm23237@josai.ac.jp>
2026-07-13 12:13:35 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,20 +2,25 @@ alert('Hello, Ousei!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 
-/*2の平方根と3の平方根の輪を計算する*/
+/*2の平方根と3の平方根の和を計算する*/
 const sum = Math.sqrt(2) + Math.sqrt(3);
+alert(sum);
+
 /*1を0で割る計算をする*/
-const result = 1 / 0;
+const result1 = 1 / 0;
+alert(result1)
+
 /*0を0で割る計算をする*/
-const result = 0 / 0;
+const result2 = 0 / 0;
+alert(result2)
 
 /*以下、検証済み*/
 
-alert(const sum = (2+2));
-alert(const sum = ('2'+'3'));
-alert(const sum = (Number('2')+Number('3')));
-alert(const sum = ('2'+3));
-alert(const sum = (Number('2')+3));
+alert(2+2);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
 
 /*2026/07/13 情報学特講Ⅰ*/
 /*上記コードを再活性化・修正*/
