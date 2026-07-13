6143c80650db5cb43380da7fec122b3bbf093f1c
zm23237 <zm23237@josai.ac.jp>
2026-07-13 11:51:33 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -3,9 +3,12 @@ alert(1+1);
 
 /*以下、検証済み*/
 
-/*alert(Math.sprt(1+1));*/
-/*alert(2+2);*/
-/*alert('2'+'3');*/
-/*alert(Number('2')+Number('3'));*/
-/*alert('2'+3);*/
-/*alert(Number('2')+3);*/
+alert(Math.sprt(1+1));
+alert(2+2);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
+
+/*2026/07/13 情報学特講Ⅰ*/
+/*上記コードを再活性化しました*/
